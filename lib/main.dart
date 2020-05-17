4dfb51d89c6e1971548e91df6c72292c95a9fc34
Iago Novoa Antunes <inovoaan@everis.com>
2020-05-17 16:17:33 -0300

    Inicio do projeto, introduzindo sobre o basico de flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+// Importação desse pacote é necessário pois nos dá o acesso aos widgets do dart
+// Tanto quanto do Material Theme
+import 'package:flutter/material.dart';
+// O método main é o principal método da aplicação, ele que complica nosso código dart
+void main() {
+  // Chamando esse método para rodar a aplicação
+  // É necessário usar um widget como parâmetro
+  runApp(
+    Container(
+      color: Colors.blue,
+    ),
+  );
+}
